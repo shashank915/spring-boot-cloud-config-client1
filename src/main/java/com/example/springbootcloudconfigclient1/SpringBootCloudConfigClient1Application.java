e8b98a17d935563c63612bd0fa51e12a478e9e79
Shashank <shashank@frostinteractive.com>
2018-09-26 12:00:54 +0530

    Initial commit

---
+++ src/main/java/com/example/springbootcloudconfigclient1/SpringBootCloudConfigClient1Application.java
@@ -0,0 +1,17 @@
+package com.example.springbootcloudconfigclient1;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudConfigClient1Application {
+
+    @Value("${message}")
+    private static String message;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudConfigClient1Application.class, args);
+        System.out.println("Configuration Message: " + message);
+    }
+}
